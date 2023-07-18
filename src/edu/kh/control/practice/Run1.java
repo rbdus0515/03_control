c9c1dcd7fe983f45a5b84b95bbda7667244edc0b
rbdus0515 <dnqkd707@gmail.com>
2023-07-18 15:51:55 +0900

    upup

--- src/edu/kh/control/practice/Run1.java
+++ src/edu/kh/control/practice/Run1.java
@@ -7,7 +7,7 @@ public class Run1 {
 		ConditionPractice condition = new ConditionPractice();
 		
 		//condition.practice1();
-		condition.practice2();
+		//condition.practice2();
 		//condition.practice3();
 		//condition.practice4();
 		//condition.practice5();
